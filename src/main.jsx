8e397eaf1de6f29a54b3dc8798119f7059e6c289
devsilva <jerson.silva99@hotmail.com>
2023-02-03 23:17:00 -0500

    feat: Upload initial files

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from 'App'
+
+import { $ } from 'utils/dom'
+
+import 'index.css'
+
+ReactDOM.createRoot($('#root')).render(
+  <App />
+)
